b3212461d5b2a6a85d0dd67bab088207f87e7d79
Reda Lemeden <git@redalemeden.com>
2020-04-28 01:14:30 +0200

    Make decoder initializer public

--- Sources/QueryStringDecoder.swift
+++ Sources/QueryStringDecoder.swift
@@ -1,39 +1,9 @@
 import Foundation
 import NIO
 
-public enum FormContent {
-  public struct URLEncoded {
-    public private(set) var storage = [String: String]()
-
-    static func parse(_ string: String) -> [String: String] {
-      let percentEncoded = string.replacingOccurrences(of: "+", with: "%20")
-      let pairs: [(Substring, Substring)] = percentEncoded
-        .split(separator: "&")
-        .map {
-          let pair = $0.split(separator: "=", maxSplits: 1, omittingEmptySubsequences: false)
-          return (pair[0], pair[1])
-        }
-
-      return pairs.reduce([:]) { result, pair in
-        var newResult = result
-        newResult[String(pair.0)] = pair.1.removingPercentEncoding ?? String(pair.1)
-        return newResult
-      }
-    }
-  }
-}
-
-extension FormContent.URLEncoded: ExpressibleByStringLiteral {
-  public init(_ string: String) {
-    self = .init(storage: FormContent.URLEncoded.parse(string))
-  }
-
-  public init(stringLiteral: String) {
-    self = .init(storage: FormContent.URLEncoded.parse(stringLiteral))
-  }
-}
-
 public final class QueryStringDecoder {
+  public init() {}
+  
   public func decode<T>(
     _ decodable: T.Type,
     from body: ByteBuffer
