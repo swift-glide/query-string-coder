ae380f5b1a3a6d335b832d66b3226da885c8f3e1
Reda Lemeden <git@redalemeden.com>
2020-07-22 13:33:21 +0200

    Remove explicit NIO dependency

--- Package.swift
+++ Package.swift
@@ -7,18 +7,14 @@ let package = Package(
     .macOS(.v10_15)
   ],
   products: [
-    // Products define the executables and libraries produced by a package, and make them visible to other packages.
     .library(
       name: "QueryStringCoder",
       targets: ["QueryStringCoder"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/SwiftGlide/glide", from: "0.0.9"),
-    .package(url: "https://github.com/apple/swift-nio", from: "2.19.0")
+    .package(url: "https://github.com/SwiftGlide/glide", from: "0.0.9")
   ],
   targets: [
-    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-    // Targets can depend on other targets in this package, and on products in packages which this package depends on.
     .target(
       name: "QueryStringCoder",
       dependencies: [
